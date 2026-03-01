6fb689f8fe042f76f871e498ca8b953a76a106cf
M.can <caglayan.mertcan37@gmail.com>
2026-03-01 17:46:10 +0300

    add edition bar, update title,font

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import React from 'react'
 import './App.css'
+import EditionBar from './layout/editionBar/EditionBar'
 import Header from './layout/header/Header'
 
 function App() {
   return (
-    <>
+    <React.Fragment>
       <Header></Header>
-    </>
+      <EditionBar></EditionBar>
+    </React.Fragment>
   )
 }
 
