0fded67343086fe74ce11f6436294a1794b56fa8
M.can <caglayan.mertcan37@gmail.com>
2026-03-22 19:33:42 +0300

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 import EditionBar from './layout/editionBar/EditionBar'
 import Header from './layout/header/Header'
+import Footer from './layout/footer/Footer'
 
 function App() {
   return (
     <React.Fragment>
       <Header></Header>
       <EditionBar></EditionBar>
+      <Footer></Footer>
     </React.Fragment>
   )
 }
