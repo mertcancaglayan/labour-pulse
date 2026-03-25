5a8e9883ba2c08538362f2af11fd9ae411d80585
M.can <caglayan.mertcan37@gmail.com>
2026-03-25 21:32:24 +0300

    Inflation Chart added

--- src/App.tsx
+++ src/App.tsx
@@ -2,53 +2,53 @@ import './App.css'
 import EditionBar from './layout/editionBar/EditionBar'
 import Header from './layout/header/Header'
 import Footer from './layout/footer/Footer'
-import React, { useEffect, useState } from 'react'
-import { getData } from './services/getRawData'
-import type { TransformedDataI } from './services/transformer'
+import React from 'react'
+import InflationChart from './components/charts/InflationChart'
+
 
 
 function App() {
   // const [laborData, setLaborData] = useState<EmploymentData[]>([])
   // const [ecoData, setEcoData] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
-  const [data, setData] = useState<Record<number, TransformedDataI>>([])
+  // const [isLoading, setIsLoading] = useState(true)
+  // const [data, setData] = useState<Record<number, TransformedDataI>>([])
 
-  // if (isLoading) return <div>LOADING</div>
+  // // if (isLoading) return <div>LOADING</div>
 
-  useEffect(() => {
-    const init = async () => {
-      try {
-        const response = await getData()
-        const result = response
+  // useEffect(() => {
+  //   const init = async () => {
+  //     try {
+  //       const response = await getData()
+  //       const result = response
 
-        setData(result)
-        setIsLoading(false)
-      } catch (error) {
-        console.error("Initialization failed", error)
-        setIsLoading(false)
-      }
-    }
+  //       setData(result)
+  //       setIsLoading(false)
+  //     } catch (error) {
+  //       console.error("Initialization failed", error)
+  //       setIsLoading(false)
+  //     }
+  //   }
 
-    init()
-  }, [])
+  //   init()
+  // }, [])
 
-  useEffect(() => {
-    console.log(data);
-  }, [data])
+  // useEffect(() => {
+  //   console.log(data);
+  // }, [data])
 
-  if (isLoading) {
-    return (
-      <div style={{ background: 'var(--bg)', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-        <h2 style={{ fontFamily: 'Bebas Neue', letterSpacing: '2px' }}>LOADING DATA...</h2>
-      </div>
-    )
-  }
+  // if (isLoading) {
+  //   return (
+  //     <div style={{ background: 'var(--bg)', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+  //       <h2 style={{ fontFamily: 'Bebas Neue', letterSpacing: '2px' }}>LOADING DATA...</h2>
+  //     </div>
+  //   )
+  // }
 
   return (
     <React.Fragment>
       <Header></Header>
       <EditionBar></EditionBar>
-
+      <InflationChart></InflationChart>
 
       <Footer></Footer>
     </React.Fragment>
--- src/components/charts/InflationChart.tsx
+++ src/components/charts/InflationChart.tsx
@@ -1,7 +1,94 @@
+import { useEffect, useState } from "react"
+import { getData } from "../../services/getRawData"
+import type { TransformedDataI } from "../../services/transformer"
+import {
+    Chart as ChartJS,
+    defaults,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+    elements,
+} from "chart.js"
+import { Line } from "react-chartjs-2"
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+)
+
+
+defaults.maintainAspectRatio = false
+defaults.responsive = true
+
 function InflationChart() {
+    const [isLoading, setIsLoading] = useState(true)
+    const [data, setData] = useState<Record<number, TransformedDataI>>({})
+
+    useEffect(() => {
+        const init = async () => {
+            try {
+                const result = await getData()
+                setData(result)
+            } catch (error) {
+                console.error("Initialization failed", error)
+            } finally {
+                setIsLoading(false)
+            }
+        }
+
+        init()
+    }, [])
+
+    if (isLoading) {
+        return (
+            <div style={{ background: 'var(--bg)', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                <h2 style={{ fontFamily: 'Bebas Neue', letterSpacing: '2px' }}>LOADING DATA...</h2>
+            </div>
+        )
+    }
+
+    const labels = Object.keys(data)
+    const inflationValues = Object.values(data).map(
+        (item) => Math.trunc(item["Headline Inflation (CPI)"])
+    )
+
     return (
         <div>
+            <Line
+                key={JSON.stringify(data)}
+                datasetIdKey="kpi-id"
+                data={{
+                    labels: labels,
+                    datasets: [
+                        {
+                            label: 'Inflation by Year',
+                            data: inflationValues,
+                            backgroundColor: "#865b0f",
+                            borderColor: "#865b0f"
+                        },
 
+                    ],
+                }}
+                options={{
+                    elements: {
+                        point: {
+                            hoverRadius: 20
+                        }
+                    }
+                }}
+            >
+
+
+            </Line>
         </div>
     )
 }
